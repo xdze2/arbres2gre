4a907a1d50f76556f380e1a17ddd44ad6c35545c
etienne <xdze2.me@gmail.com>
2017-12-24 19:22:47 +0100

    working query

--- wikidata.js
+++ wikidata.js
@@ -3,53 +3,68 @@ console.log('hello !');
 var app = new Vue({
   el: '#app',
   data: {
-    message: 'Hello Vue!'
+    message: 'Hello Vue!',
+    data:''
   },
   methods: {
-    showinfo: function ( sciname ) {
+    askinfo: function ( sciname ) {
         this.message = sciname;
         console.log('show ' + sciname);
 
-        this.searchquery( sciname )
+        var res = this.searchquery( sciname )
+
+    },
+    showinfo: function( response ){
+        this.data = response.data;
     },
     searchquery: function ( texttosearch ) {
-        var config = {
-          headers: { 'crossDomain': true, 'origin':'*' },
-        };
+        var _this = this;
+        axios.get('https://fr.wikipedia.org/w/api.php?origin=*', {
+            params:{
+                action:'opensearch',
+                search: texttosearch,
+                namespace: "0",
+                format: 'json',
+                limit: 5}
+        }).then(function (response) {
+            // console.log(response);
+            _this.showinfo( response );
+          })
+          .catch(function (error) {
+              console.log('request error');
+            console.log(error);
+          });
 
-        // axios.get('https://fr.wikipedia.org/w/api.php', {
+        // var data = {
         //     action:'opensearch',
         //     search: texttosearch,
-        //     namespace: "0",
-        //     format: 'jsonfm',
+        //     format: 'json',
         //     limit: 5
-        // }, config)
-        var data = {
-            action:'opensearch',
-            search: texttosearch,
-            namespace: "0",
-            format: 'jsonfm',
-            limit: 5
-        };
-        var url = 'https://fr.wikipedia.org/w/api.php';
-        fetch(url, {
-          method: "POST",
-          body: JSON.stringify(data),
-          headers: {
-            "Content-Type": "application/json", "Origin":'*',
-        },
-          mode: 'no-cors'
-        }).then(function(response) {
-          response.status     //=> number 100–599
-          response.statusText //=> String
-          response.headers    //=> Headers
-          response.url        //=> String
-
-          return response.text()
-        }, function(error) {
-          error.message
-          console.log(error);
-        })
+        //     };
+        // var url = 'https://fr.wikipedia.org/w/api.php?origin=*';
+        // fetch(url, {
+        //   method: "POST",
+        //   // body: JSON.stringify(data),
+        //   body: 'action=opensearch&search=Eisntein',
+        //   headers: {
+        //     "Content-Type": "application/json",
+        // },
+        //  mode: 'cors'
+        // }).then(function(response) {
+        //   // response.status     //=> number 100–599
+        //   // response.statusText //=> String
+        //   // response.headers    //=> Headers
+        //   // response.url        //=> String
+        //   console.log(response);
+        //
+        //
+        //   return response.text()
+        //
+        // }, function(error) {
+        //     console.log('error');
+        //   error.message
+        //   console.log(error);
+        // })
 
     }
  }
